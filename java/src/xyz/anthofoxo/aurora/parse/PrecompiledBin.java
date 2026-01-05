69d76d7b8ca3c9524f2213dc69c08d0931b5d44a
JsonX05 <discord.twister05@gmail.com>
2026-01-05 06:33:43 +0000

    commented precompiledbins, can start removing some

--- java/src/xyz/anthofoxo/aurora/parse/PrecompiledBin.java
+++ java/src/xyz/anthofoxo/aurora/parse/PrecompiledBin.java
@@ -88,68 +88,68 @@ public final class PrecompiledBin {
 	public static List<ObjectDeclaration> getDeclarations() {
 		// @formatter:off
 		return Arrays.asList(
-				new ObjectDeclaration(DeclarationType.Mesh,          "web_points.mesh"),
+				new ObjectDeclaration(DeclarationType.Mesh,          "web_points.mesh"), //1
 				new ObjectDeclaration(DeclarationType.SequinDrawer,  "sequin.drawer"), // 1?
 				new ObjectDeclaration(DeclarationType.TraitAnim,     "gamma_modulation.anim"),
-				new ObjectDeclaration(DeclarationType.Mat,           "skybox_src.mat"),
-				new ObjectDeclaration(DeclarationType.Mat,           "skybox_subtract.mat"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "skybox_rotation.anim"),
+				new ObjectDeclaration(DeclarationType.Mat,           "skybox_src.mat"), //1
+				new ObjectDeclaration(DeclarationType.Mat,           "skybox_subtract.mat"), //1
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "skybox_rotation.anim"), //1
 				new ObjectDeclaration(DeclarationType.Flow,          "level_start.flow"),
-				new ObjectDeclaration(DeclarationType.Flow,          "beneath_ice.flow"),
+				new ObjectDeclaration(DeclarationType.Flow,          "beneath_ice.flow"), //1 //sfx
 				new ObjectDeclaration(DeclarationType.Env,           "world.env"), // 1
 				new ObjectDeclaration(DeclarationType.TraitAnim,     "skybox_colors.anim"),  // 1
-				new ObjectDeclaration(DeclarationType.Flow,          "skybox_colors.flow"), // 1
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "boss_colors.anim"),
-				new ObjectDeclaration(DeclarationType.Flow,          "boss_colors.flow"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "crakhed_colors.anim"),
-				new ObjectDeclaration(DeclarationType.Flow,          "crakhed_colors.flow"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "level_colors.anim"),
-				new ObjectDeclaration(DeclarationType.Flow,          "level_colors.flow"),
-				new ObjectDeclaration(DeclarationType.Flow,          "level_colors_switch.flow"),
-				new ObjectDeclaration(DeclarationType.Flow,          "turn_anticipation.flow"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "pyramid_colors.anim"),
-				new ObjectDeclaration(DeclarationType.Flow,          "pyramid_colors.flow"),
-				new ObjectDeclaration(DeclarationType.Flow,          "ending_sequence.flow"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "diamond_colors.anim"),
-				new ObjectDeclaration(DeclarationType.Flow,          "diamond_colors.flow"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "ending_sequence_colors.anim"),
-				new ObjectDeclaration(DeclarationType.PathDecorator, "start_cap_tunnel_chrome.dec"),
+				new ObjectDeclaration(DeclarationType.Flow,          "skybox_colors.flow"), // 1 skybox colors
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "boss_colors.anim"), //1
+				new ObjectDeclaration(DeclarationType.Flow,          "boss_colors.flow"), //1 skybox colors
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "crakhed_colors.anim"), //1
+				new ObjectDeclaration(DeclarationType.Flow,          "crakhed_colors.flow"), //1 skybox colors
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "level_colors.anim"), //1
+				new ObjectDeclaration(DeclarationType.Flow,          "level_colors.flow"), //1 skybox colors
+				new ObjectDeclaration(DeclarationType.Flow,          "level_colors_switch.flow"), //1
+				new ObjectDeclaration(DeclarationType.Flow,          "turn_anticipation.flow"), //1 sfx
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "pyramid_colors.anim"), //1
+				new ObjectDeclaration(DeclarationType.Flow,          "pyramid_colors.flow"), //1 skybox colors
+				new ObjectDeclaration(DeclarationType.Flow,          "ending_sequence.flow"), //1 (level 9 ending)
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "diamond_colors.anim"), //1
+				new ObjectDeclaration(DeclarationType.Flow,          "diamond_colors.flow"), //1 skybox colors
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "ending_sequence_colors.anim"), //1
+				new ObjectDeclaration(DeclarationType.PathDecorator, "start_cap_tunnel_chrome.dec"), //1 probably needed
 				new ObjectDeclaration(DeclarationType.Flow,          "fade.flow"), // 1
-				new ObjectDeclaration(DeclarationType.Flow,          "ending_sequence_colors.flow"),
+				new ObjectDeclaration(DeclarationType.Flow,          "ending_sequence_colors.flow"), //1
 				new ObjectDeclaration(DeclarationType.Path,          "chrome_rail_20.path"),
 				new ObjectDeclaration(DeclarationType.Path,          "chrome_rail_stretched.path"),
-				new ObjectDeclaration(DeclarationType.Mat,           "level_arms.mat"),
-				new ObjectDeclaration(DeclarationType.Mesh,          "level_arms_A.mesh"),
-				new ObjectDeclaration(DeclarationType.Path,          "level_arms_A.path"),
-				new ObjectDeclaration(DeclarationType.Tex2D,         "gradient4.tex"),
-				new ObjectDeclaration(DeclarationType.Mat,           "tunnel_chrome.mat"),
-				new ObjectDeclaration(DeclarationType.Mesh,          "tunnel_chrome.mesh"),
-				new ObjectDeclaration(DeclarationType.Path,          "tunnel_chrome.path"),
-				new ObjectDeclaration(DeclarationType.Mat,           "tunnel_chrome_dark.mat"),
-				new ObjectDeclaration(DeclarationType.Path,          "tunnel_chrome_dark.path"),
-				new ObjectDeclaration(DeclarationType.Mesh,          "tunnel_chrome_dark.mesh"),
-				new ObjectDeclaration(DeclarationType.Path,          "landscape01.path"), // 1
-				new ObjectDeclaration(DeclarationType.Mesh,          "landscape01.mesh"), // 1
-				new ObjectDeclaration(DeclarationType.Tex2D,         "landscape1.tex"), // 1
-				new ObjectDeclaration(DeclarationType.Mat,           "landscape.mat"), // 1
-				new ObjectDeclaration(DeclarationType.Flow,          "extreme_path_sx.flow"),
-				new ObjectDeclaration(DeclarationType.SequinPulse,   "angle_frac_reverse.pulse"),
-				new ObjectDeclaration(DeclarationType.SequinPulse,   "angle_frac.pulse"),
-				new ObjectDeclaration(DeclarationType.Tex2D,         "psychedelic10_BW.tex"),
-				new ObjectDeclaration(DeclarationType.Mat,           "psychedelic10_BW.mat"),
-				new ObjectDeclaration(DeclarationType.Mesh,          "occlusion_tube3_psych_BW.mesh"),
-				new ObjectDeclaration(DeclarationType.Path,          "occlusion_tube03_psych_BW.path"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "tex_scroll_level9.anim"),
-				new ObjectDeclaration(DeclarationType.TraitAnim,     "pyramid_invert.anim"),
-				new ObjectDeclaration(DeclarationType.Mesh,          "lattice.mesh"),
-				new ObjectDeclaration(DeclarationType.Path,          "lattice.path"),
-				new ObjectDeclaration(DeclarationType.Mat,           "lattice_chrome.mat"),
-				new ObjectDeclaration(DeclarationType.Tex2D,         "gradient_lattice_chrome.tex"),
-				new ObjectDeclaration(DeclarationType.Tex2D,         "lattice_emissive.tex"),
-				new ObjectDeclaration(DeclarationType.Mesh,          "web.mesh"),
-				new ObjectDeclaration(DeclarationType.Path,          "web.path"),
-				new ObjectDeclaration(DeclarationType.Path,          "web_points.path"),
-				new ObjectDeclaration(DeclarationType.Flow,          "boss_frac_pulse.flow")
+				new ObjectDeclaration(DeclarationType.Mat,           "level_arms.mat"), //1
+				new ObjectDeclaration(DeclarationType.Mesh,          "level_arms_A.mesh"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "level_arms_A.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.Tex2D,         "gradient4.tex"), //1?
+				new ObjectDeclaration(DeclarationType.Mat,           "tunnel_chrome.mat"), //1
+				new ObjectDeclaration(DeclarationType.Mesh,          "tunnel_chrome.mesh"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "tunnel_chrome.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.Mat,           "tunnel_chrome_dark.mat"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "tunnel_chrome_dark.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.Mesh,          "tunnel_chrome_dark.mesh"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "landscape01.path"), //
+				new ObjectDeclaration(DeclarationType.Mesh,          "landscape01.mesh"), //		these landscape tunnels are debug paths
+				new ObjectDeclaration(DeclarationType.Tex2D,         "landscape1.tex"), //		we probably don't need these. they look terrible in game
+				new ObjectDeclaration(DeclarationType.Mat,           "landscape.mat"), //
+				new ObjectDeclaration(DeclarationType.Flow,          "extreme_path_sx.flow"), // this sounds interesting. should look into what this does
+				new ObjectDeclaration(DeclarationType.SequinPulse,   "angle_frac_reverse.pulse"), //1
+				new ObjectDeclaration(DeclarationType.SequinPulse,   "angle_frac.pulse"), //1 boss fx
+				new ObjectDeclaration(DeclarationType.Tex2D,         "psychedelic10_BW.tex"), //1
+				new ObjectDeclaration(DeclarationType.Mat,           "psychedelic10_BW.mat"), //1
+				new ObjectDeclaration(DeclarationType.Mesh,          "occlusion_tube3_psych_BW.mesh"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "occlusion_tube03_psych_BW.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "tex_scroll_level9.anim"), //1?
+				new ObjectDeclaration(DeclarationType.TraitAnim,     "pyramid_invert.anim"), //1
+				new ObjectDeclaration(DeclarationType.Mesh,          "lattice.mesh"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "lattice.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.Mat,           "lattice_chrome.mat"), //1
+				new ObjectDeclaration(DeclarationType.Tex2D,         "gradient_lattice_chrome.tex"), //1
+				new ObjectDeclaration(DeclarationType.Tex2D,         "lattice_emissive.tex"), //1
+				new ObjectDeclaration(DeclarationType.Mesh,          "web.mesh"), //1
+				new ObjectDeclaration(DeclarationType.Path,          "web.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.Path,          "web_points.path"), //1 tunnel
+				new ObjectDeclaration(DeclarationType.Flow,          "boss_frac_pulse.flow") //1 boss fx
 			);
 		// @formatter:on
 	}
