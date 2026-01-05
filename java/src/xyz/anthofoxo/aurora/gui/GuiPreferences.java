1afb70828ed940d2b0b97d89283b5f2aa05cf6c6
AnthoFoxo <anthofoxo@gmail.com>
2026-01-05 01:14:32 -0500

    update mod launcher and search path panel duplication

--- java/src/xyz/anthofoxo/aurora/gui/GuiPreferences.java
+++ java/src/xyz/anthofoxo/aurora/gui/GuiPreferences.java
@@ -7,8 +7,14 @@ import java.io.IOException;
 import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
 import imgui.ImGui;
+import imgui.ImVec2;
+import imgui.ImVec4;
+import imgui.flag.ImGuiCol;
+import imgui.flag.ImGuiStyleVar;
 import imgui.type.ImBoolean;
+import xyz.anthofoxo.aurora.TextureRegistry;
 import xyz.anthofoxo.aurora.UserConfig;
+import xyz.anthofoxo.aurora.gfx.Font;
 
 public final class GuiPreferences {
 	private GuiPreferences() {
@@ -16,6 +22,59 @@ public final class GuiPreferences {
 
 	public static ImBoolean visible = new ImBoolean(false);
 	public static ImBoolean unlockPractice = new ImBoolean(UserConfig.isUnlockPractice());
+
+	public static void modSearchPathsPanel() {
+		if (ImGui.button("Add Search Path")) {
+			String path = TinyFileDialogs.tinyfd_selectFolderDialog("Mod Search Path", null);
+
+			if (path != null) {
+				UserConfig.modPaths.add(path);
+				UserConfig.save();
+				ModLauncher.reloadList();
+			}
+		}
+
+		ImGui.sameLine();
+		ImGui.pushFont(Font.getFont("defaultsmall"));
+		ImGui.text("Any levels found in the listed paths will appear in the level list.");
+		ImGui.popFont();
+
+		int removeIdx = -1;
+
+		for (int i = 0; i < UserConfig.modPaths.size(); ++i) {
+			ImGui.pushStyleVar(ImGuiStyleVar.ItemSpacing, new ImVec2(3f, 3f));
+			var buttonremove = TextureRegistry.get("button_icons/icon-remove.png");
+			ImGui.pushID(i);
+			ImGui.pushStyleColor(ImGuiCol.Button, new ImVec4(0.4f, 0, 0, 1));
+			ImGui.pushStyleColor(ImGuiCol.ButtonHovered, new ImVec4(1f, 0, 0, 1));
+			if (ImGui.imageButton("buttonremove", buttonremove.getHandle(), 16, 16)) removeIdx = i;
+			ImGui.popStyleColor(2);
+
+			ImGui.sameLine();
+
+			var buttonfolder = TextureRegistry.get("button_icons/icon-openfolder.png");
+			if (ImGui.imageButton("buttonfolder", buttonfolder.getHandle(), 16, 16)) {
+				try {
+					Desktop.getDesktop().open(new File(UserConfig.modPaths.get(i)));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			ImGui.popStyleVar();
+
+			ImGui.sameLine();
+			ImGui.pushFont(Font.getFont("consolas"));
+			ImGui.textUnformatted(UserConfig.modPaths.get(i));
+			ImGui.popFont();
+			ImGui.popID();
+		}
+
+		if (removeIdx != -1) {
+			UserConfig.modPaths.remove(removeIdx);
+			UserConfig.save();
+			ModLauncher.reloadList();
+		}
+	}
 
 	public static void draw() {
 		if (!visible.get()) return;
@@ -41,43 +100,7 @@ public final class GuiPreferences {
 
 		ImGui.separatorText("Mod Search Paths");
 
-		int removeIdx = -1;
-
-		for (int i = 0; i < UserConfig.modPaths.size(); ++i) {
-			ImGui.pushID(i);
-			if (ImGui.smallButton("x")) removeIdx = i;
-
-			ImGui.sameLine();
-
-			if (ImGui.smallButton("Open")) {
-				try {
-					Desktop.getDesktop().open(new File(UserConfig.modPaths.get(i)));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			ImGui.sameLine();
-			ImGui.textUnformatted(UserConfig.modPaths.get(i));
-			ImGui.popID();
-		}
-
-		if (removeIdx != -1) {
-			UserConfig.modPaths.remove(removeIdx);
-			UserConfig.save();
-			ModLauncher.reloadList();
-		}
-
-		if (ImGui.button("Add Search Path")) {
-			String path = TinyFileDialogs.tinyfd_selectFolderDialog("Mod Search Path", null);
-
-			if (path != null) {
-				UserConfig.modPaths.add(path);
-				UserConfig.save();
-				ModLauncher.reloadList();
-			}
-
-		}
+		modSearchPathsPanel();
 
 		ImGui.end();
 	}
--- java/src/xyz/anthofoxo/aurora/gui/ModLauncher.java
+++ java/src/xyz/anthofoxo/aurora/gui/ModLauncher.java
@@ -1,14 +1,9 @@
 package xyz.anthofoxo.aurora.gui;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
-import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
 import imgui.ImGui;
 import imgui.ImGuiTextFilter;
@@ -161,58 +156,7 @@ public class ModLauncher {
 			ImGui.textUnformatted(UserConfig.thumperPath());
 
 			ImGui.separatorText("Mod Search Paths");
-
-			if (ImGui.button("Add Search Path")) {
-				String path = TinyFileDialogs.tinyfd_selectFolderDialog("Mod Search Path", null);
-
-				if (path != null) {
-					UserConfig.modPaths.add(path);
-					UserConfig.save();
-					ModLauncher.reloadList();
-				}
-
-			}
-			ImGui.sameLine();
-			ImGui.pushFont(Font.getFont("defaultsmall"));
-			ImGui.text("Any levels found in the listed paths will appear in the level list below.");
-			ImGui.popFont();
-
-			int removeIdx = -1;
-
-			for (int i = 0; i < UserConfig.modPaths.size(); ++i) {
-				ImGui.pushStyleVar(ImGuiStyleVar.ItemSpacing, new ImVec2(3f, 3f));
-				var buttonremove = TextureRegistry.get("button_icons/icon-remove.png");
-				ImGui.pushID(i);
-				ImGui.pushStyleColor(ImGuiCol.Button, new ImVec4(0.4f, 0, 0, 1));
-				ImGui.pushStyleColor(ImGuiCol.ButtonHovered, new ImVec4(1f, 0, 0, 1));
-				if (ImGui.imageButton("buttonremove", buttonremove.getHandle(), 16, 16)) removeIdx = i;
-				ImGui.popStyleColor(2);
-
-				ImGui.sameLine();
-
-				var buttonfolder = TextureRegistry.get("button_icons/icon-openfolder.png");
-				if (ImGui.imageButton("buttonfolder", buttonfolder.getHandle(), 16, 16)) {
-					try {
-						Desktop.getDesktop().open(new File(UserConfig.modPaths.get(i)));
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-				ImGui.popStyleVar();
-
-				ImGui.sameLine();
-				ImGui.pushFont(Font.getFont("consolas"));
-				ImGui.textUnformatted(UserConfig.modPaths.get(i));
-				ImGui.popFont();
-				ImGui.popID();
-			}
-
-			if (removeIdx != -1) {
-				UserConfig.modPaths.remove(removeIdx);
-				UserConfig.save();
-				ModLauncher.reloadList();
-			}
-			///
+			GuiPreferences.modSearchPathsPanel();
 
 			ImGui.pushStyleVar(ImGuiStyleVar.ItemSpacing, new ImVec2(0, 10));
 			ImGui.text(" ");
@@ -382,7 +326,7 @@ public class ModLauncher {
 							ImGui.separator();
 							ImGui.pushStyleColor(ImGuiCol.Separator, selected.tcl.railsColor.toImVec4wxyz(null));
 							ImGui.separator();
-							ImGui.pushStyleColor(ImGuiCol.Separator, selected.tcl.railsGlowColor.toImVec4wxyz(null));
+							ImGui.pushStyleColor(ImGuiCol.Separator, selected.tcl.pathColor.toImVec4wxyz(null));
 							ImGui.separator();
 							ImGui.popStyleColor(3);
 
